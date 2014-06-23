c038fa4661404b6d20527f886ed34a9c8762022b
nm777taka <nm7.taka@gmail.com>
2014-06-24 00:25:15 +0900

    beaconシングルトンクラスを追加

---
+++ BeaconGalax/GXBeacon.h
@@ -0,0 +1,13 @@
+//
+//  GXBeacon.h
+//  BeaconGalax
+//
+//  Created by 古田貴久 on 2014/06/24.
+//  Copyright (c) 2014年 古田貴久. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GXBeacon : NSObject
+
+@end
