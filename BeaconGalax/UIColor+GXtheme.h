5cdd338df87ca21f003b42799fe17d0110577d68
nm777taka <nm7.taka@gmail.com>
2014-06-25 04:52:48 +0900

    UIColorにカテゴリ(GXTheme)を追加

---
+++ BeaconGalax/UIColor+GXtheme.h
@@ -0,0 +1,16 @@
+//
+//  UIColor+GXtheme.h
+//  BeaconGalax
+//
+//  Created by 古田貴久 on 2014/06/25.
+//  Copyright (c) 2014年 古田貴久. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (GXtheme)
+
++ (UIColor *)gxGradientStartColor;
++ (UIColor *)gxGradientEndColor;
+
+@end
---
+++ BeaconGalax/UIColor+GXtheme.m
@@ -0,0 +1,24 @@
+//
+//  UIColor+GXtheme.m
+//  BeaconGalax
+//
+//  Created by 古田貴久 on 2014/06/25.
+//  Copyright (c) 2014年 古田貴久. All rights reserved.
+//
+
+#import "UIColor+GXtheme.h"
+
+@implementation UIColor (GXtheme)
+
+
++ (UIColor *)gxGradientStartColor
+{
+    return [UIColor colorWithRed:81 green:146 blue:187 alpha:1];
+}
+
++ (UIColor *)gxGradientEndColor
+{
+    return [UIColor colorWithRed:143 green:201 blue:213 alpha:1];
+}
+
+@end
