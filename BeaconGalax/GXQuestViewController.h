4aa7244eec4a265fb683d4ea3e6b7afc171e5cc0
古田貴久 <nm777.taka@gmail.com>
2014-09-08 02:05:26 +0900

    toolbarを追加

--- BeaconGalax/GXQuestViewController.h
+++ BeaconGalax/GXQuestViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface GXQuestViewController : UIViewController
+@interface GXQuestViewController : UIViewController<UITableViewDataSource,UITableViewDelegate>
 
 @end
--- BeaconGalax/GXQuestViewController.m
+++ BeaconGalax/GXQuestViewController.m
@@ -7,9 +7,13 @@
 //
 
 #import "GXQuestViewController.h"
+#import "GXHomeTableViewCell.h"
 
 @interface GXQuestViewController ()
-
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property GXHomeTableViewCell *stubCell;
+@property NSArray *textArray;
+@property NSMutableArray *objects;
 @end
 
 @implementation GXQuestViewController
@@ -26,7 +30,27 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+	// Do any additional setup after loading the view, typically from a nib.
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    
+    self.navigationItem.leftBarButtonItem = self.editButtonItem;
+    
+    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
+    self.navigationItem.rightBarButtonItem = addButton;
+    
+    _stubCell = [self.tableView dequeueReusableCellWithIdentifier:@"Cell"]; // 追加
+    
+    // 追加
+    // 文字列の配列の作成
+    _textArray = @[
+                   @"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sodales diam sed turpis mattis dictum. In laoreet porta eleifend. Ut eu nibh sit amet est iaculis faucibus.",
+                   @"initWithBitmapDataPlanes:pixelsWide:pixelsHigh:bitsPerSample:samplesPerPixel:hasAlpha:isPlanar:colorSpaceName:bitmapFormat:bytesPerRow:bitsPerPixel:",
+                   @"祇辻飴葛蛸鯖鰯噌庖箸",
+                   @"Nam in vehicula mi.",
+                   @"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+                   @"あのイーハトーヴォの\nすきとおった風、\n夏でも底に冷たさをもつ青いそら、\nうつくしい森で飾られたモーリオ市、\n郊外のぎらぎらひかる草の波。",
+                   ];
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,15 +59,92 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
+#pragma mark - private methods
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+- (void)insertNewObject:(id)sender
 {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    if (!_objects) {
+        _objects = [[NSMutableArray alloc] init];
+    }
+    
+    // 追加
+    // データ作成
+    int dataIndex = arc4random() % _textArray.count;
+    NSString *string = _textArray[dataIndex];
+    NSDate *date = [NSDate date];
+    NSDictionary *dataDictionary = @{@"string": string, @"date":date};
+    
+    // データ挿入
+    [_objects insertObject:dataDictionary atIndex:0];   // 修正
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
+    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
 }
-*/
+
+- (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath
+{
+    GXHomeTableViewCell *customCell = (GXHomeTableViewCell *)cell;
+    
+    // メインラベルに文字列を設定
+    NSDictionary *dataDictionary = _objects[indexPath.row];
+    customCell.mainLabel.text = dataDictionary[@"string"];
+    
+    // サブラベルに文字列を設定
+    NSDate *date = dataDictionary[@"date"];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.dateFormat = @"yyyy年MM月dd日 HH時mm分ss秒";
+    customCell.subLabel.text = [dateFormatter stringFromDate:date];
+}
+
+#pragma mark - Table View
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // 計測用のプロパティ"_stubCell"を使って高さを計算する
+    [self configureCell:_stubCell atIndexPath:indexPath];
+    [_stubCell layoutSubviews];
+    CGFloat height = [_stubCell.contentView systemLayoutSizeFittingSize:UILayoutFittingCompressedSize].height;
+    
+    return height + 1;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView estimatedHeightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 40.0;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _objects.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    
+    [self configureCell:cell atIndexPath:indexPath];    // 追加
+    
+    return cell;
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        [_objects removeObjectAtIndex:indexPath.row];
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+    }
+}
 
 @end
