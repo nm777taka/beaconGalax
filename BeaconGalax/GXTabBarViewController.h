a04df328ff56d9e3c6c558601f92f9f55b0c8bb0
古田貴久 <nm777.taka@gmail.com>
2014-09-20 12:09:37 +0900

    tabBarControllerを導入

---
+++ BeaconGalax/GXTabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  GXTabBarViewController.h
+//  BeaconGalax
+//
+//  Created by 古田貴久 on 2014/09/20.
+//  Copyright (c) 2014年 古田貴久. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GXTabBarViewController : UITabBarController
+
+@end
